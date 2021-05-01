02eb9d81a2e8dc14b9a6d0f81fd13a6f042fa575
Sérgio Dias <up201704889@fe.up.pt>
2021-05-01 17:05:15 +0100

    Refactored Character.cs script

--- Death Upon Us/Assets/Scripts/Character.cs
+++ Death Upon Us/Assets/Scripts/Character.cs
@@ -15,38 +15,36 @@ public class Character : MonoBehaviour
         movementDirection = 0;
         rigidBody = GetComponent<Rigidbody>();
     }
-
-    void Update () 
-     {
-        movementDirection = Input.GetAxis("Horizontal");
-         
-        if(Input.GetAxis("Mouse X") < 0)
-            transform.Rotate(Vector3.up* 2) ;
-        if(Input.GetAxis("Mouse X") > 0)
-            transform.Rotate(Vector3.up* -2f) ;
-
-        if(Input.GetKey(KeyCode.W)) {
-            transform.position += transform.forward * Time.deltaTime * movementSpeed;
-         }
-     }
-    
-
     private void FixedUpdate()
     {
-        Move();
-        Jump();
+        HandleInput();
     }
 
-    private void Move()
+    private void HandleInput()
     {
-        float horizontalMovementDirection = Input.GetAxis("Horizontal");
-        float verticalMovementDirection = -Input.GetAxis("Vertical");
-        //rigidBody.velocity += Vector3.forward * horizontalMovementDirection;//new Vector3(verticalMovementDirection * movementSpeed, rigidBody.velocity.y, horizontalMovementDirection * movementSpeed);
+        float mouseDelta = Input.GetAxis("Mouse X");
+        if (mouseDelta != 0)
+        {
+            Rotate(mouseDelta < 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            MoveForward();
+        }
+    }
+
+    private void MoveForward()
+    {
+        transform.position += transform.forward * Time.deltaTime * movementSpeed;
+    }
+
+    private void Rotate(bool clockwise)
+    {
+        transform.Rotate(Vector3.up * 2f * (clockwise ? 1 : -1));
     }
 
     private void Jump()
     {
-        Debug.Log(Input.GetButton("Jump"));
         if (Input.GetButton("Jump") && isGrounded)
         {
             rigidBody.velocity = Vector3.up * jumpForce;
