c2735bcf50ca4118ba302121af577aff326af381
Sérgio Dias <up201704889@fe.up.pt>
2021-05-22 19:00:39 +0100

    Pause menu can now be open/closed from the game scene

---
+++ Death Upon Us/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static utils.Configs;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool active;
+
+    private void Start()
+    {
+        active = false;
+        gameObject.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (active)
+            {
+                gameObject.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+            }
+            active = !active;
+        }
+    }
+}
