a3536f41fae835e1889d556364c5c3d67e2896ba
Marisa <anam.macedo98@gmail.com>
2021-05-25 23:22:43 +0100

    menus buttons assets and main menu music

--- Death Upon Us/Assets/Scripts/Menus/VolumeSlider.cs
+++ Death Upon Us/Assets/Scripts/Menus/VolumeSlider.cs
@@ -7,6 +7,9 @@ using static utils.Configs;
 
 public class VolumeSlider : MonoBehaviour
 {
+    //FMOD.Studio.EventInstance menu_theme;
+    //float MusicVolume = 0.5f;
+
     public Slider slider;
 
     void Start()
@@ -18,4 +21,9 @@ public class VolumeSlider : MonoBehaviour
         float volume = Mathf.Log10(sliderValue) * 20;
         PlayerPrefs.SetFloat("sliderValue", sliderValue);
     }
+
+   /* public void volumeChange(float newVolume)
+    {
+
+    }*/
 }
