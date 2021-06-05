75f3aad9e189c30ceb8d1f35a04361473ffa048e
Sérgio Dias <up201704889@fe.up.pt>
2021-06-05 12:46:49 +0100

    Added trigger to display the cutscene

--- Death Upon Us/Assets/Scripts/CutScenes/FinalCutScene.cs
+++ Death Upon Us/Assets/Scripts/CutScenes/FinalCutScene.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
 using static utils.Configs;
 
 public class FinalCutScene : MonoBehaviour
 {
     public GameObject rotationCenter;
-    private float cameraHeight;
 
     public void Start() {
-        cameraHeight = 20f;
-        //transform.position = rotationCenter.transform.position + transform.up * cameraHeight;
+        gameObject.SetActive(false);
     }
 
     public void Update() {
         transform.LookAt(rotationCenter.transform);
-        //transform.Rotate(0, CutSceneSpeed, 0);
         transform.RotateAround(rotationCenter.transform.position, Vector3.up, CutSceneSpeed * Time.deltaTime);
     }
 }
