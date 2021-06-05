6ac3aca71d1aa181a42698e1b00b52a413770755
Sérgio Dias <up201704889@fe.up.pt>
2021-06-05 17:09:35 +0100

    Disabled characters canvas when cutscene is played

--- Death Upon Us/Assets/Scripts/CutScenes/CutSceneTrigger.cs
+++ Death Upon Us/Assets/Scripts/CutScenes/CutSceneTrigger.cs
@@ -7,12 +7,21 @@ using static utils.Configs;
 public class CutSceneTrigger : MonoBehaviour
 {
     public GameObject cutSceneCamera;
+    public Canvas boyCanvas, girlCanvas;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player")) {
             cutSceneCamera.SetActive(true);
+            StartCoroutine(DisableCharacters());   
         }
+    }
+
+    public IEnumerator DisableCharacters()
+    {
+        yield return new WaitForSeconds(0.1f);
+        boyCanvas.enabled = false;
+        girlCanvas.enabled = false;
     }
 }
 
