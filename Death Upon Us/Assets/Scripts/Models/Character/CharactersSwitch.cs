dd3cc9ce38f95ba11b4cdec947c6b44bd7514c05
AndreMoutas <47751865+AndreMoutas@users.noreply.github.com>
2021-06-14 03:17:23 +0100

    Imported some models
    WiP: Switch models

--- Death Upon Us/Assets/Scripts/Models/Character/CharactersSwitch.cs
+++ Death Upon Us/Assets/Scripts/Models/Character/CharactersSwitch.cs
@@ -70,4 +70,9 @@ public class CharactersSwitch : MonoBehaviour
             this.girl.transform.localScale = new Vector3(0f, 2.5f, 0f);
         }
     }
+
+    public bool IsGirl()
+    {
+        return isGirl;
+    }
 }
